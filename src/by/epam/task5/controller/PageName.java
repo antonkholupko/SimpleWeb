0a0c98bdbb641365a813c9db47526ff76631423d
antonkholupko <toxa94yes@gmail.com>
2016-04-26 14:00:09 +0300

    1

--- src/by/epam/task5/controller/PageName.java
+++ src/by/epam/task5/controller/PageName.java
@@ -6,5 +6,4 @@ public final class PageName {
     public static final String ERROR_PAGE = "WEB-INF/jsp/error.jsp";
     public static final String REGISTRATION_PAGE = "WEB-INF/jsp/registration.jsp";
     public static final String USER_PAGE_AFT_REG = "WEB-INF/jsp/page_aft_reg.jsp";
-    public static final String PAGE_AFT_REGISTRATION = "WEB-INF/jsp/page_aft_reg.jsp";
 }
